f1116ee2ad39e7194866739264854309798a9577
Ruben Marcus <ruben@rubenmarcus.dev>
2022-07-10 23:22:16 +0100

    Hacker Rank React Kanban

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./App.css";
+import KanbanBoard from "./components/kanban-board/index.js";
+import "h8k-components";
+
+const title = "Kanban Board";
+
+class App extends Component {
+  render() {
+    console.log(this.props.tasks, "tasks");
+    return (
+      <div>
+        <h8k-navbar header={title}></h8k-navbar>
+        <KanbanBoard tasks={this.props.tasks} />
+      </div>
+    );
+  }
+}
+
+export default App;
